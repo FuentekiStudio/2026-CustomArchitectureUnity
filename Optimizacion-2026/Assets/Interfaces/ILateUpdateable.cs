c9554ee8279a90c2af16a4320d0a444204d3d03a
Demian <demianlozanoriedel@gmail.com>
2026-06-05 09:50:53 -0300

    [NEW] Custom Update Manager
    [ADD] CustomUpdateManager and methods:
    - Update
    - Fixed Update
    - Late Update
    
    [ADD] Interfaces for updateable scripts:
    - IUpdateable
    - IFixedUpdateable
    - ILateUpdateable
    
    The interfaces contains the only method:
    public void Name(float tick)

---
+++ Optimizacion-2026/Assets/Interfaces/ILateUpdateable.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public interface ILateUpdateable
+{
+    public void LateUpdate(float tick);
+}
---
+++ Optimizacion-2026/Assets/Interfaces/IUpdateable.cs
@@ -0,0 +1,8 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public interface IUpdateable
+{
+    public void Update(float tick);
+    
+}
