2078bfafde527dafbdec45a1509cf381dcd404b5
MoNaRiX1806 <fran.simonazzi@gmail.com>
2026-06-11 21:38:41 -0300

    [ADD] Base gameObjects for reference and enemies controller concepts

--- Optimizacion-2026/Assets/Scripts/CustomUpdateManager.cs
+++ Optimizacion-2026/Assets/Scripts/CustomUpdateManager.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class CustomUpdateManager : MonoBehaviour
 {
@@ -7,45 +9,96 @@ public class CustomUpdateManager : MonoBehaviour
     public ILateUpdateable[] lateUpdateableList;
 
     
+    private List<IUpdateable> updateables = new List<IUpdateable>();
+    private List<IUpdateable> updateablesToRemove = new List<IUpdateable>();
+
+    private List<IFixedUpdateable> fixedUpdateables = new List<IFixedUpdateable>();
+    private List<IFixedUpdateable> fixedUpdateablesToRemove = new List<IFixedUpdateable>();
+
+    private List<ILateUpdateable> lateUpdateables = new List<ILateUpdateable>();
+    private List<ILateUpdateable> lateUpdateablesToRemove = new List<ILateUpdateable>();
+
 
     void Start()
     {
-        CLogger.Log("CustomUpdateManager");
+        CLogger.Log("CustomUpdateManager Init");
+
+
     }
 
     
     void Update()
     {
-        foreach (var updateable in updateableList)
+        foreach (IUpdateable updateable in updateables)
         {
             updateable.Update(Time.deltaTime);
         }
+        foreach (IUpdateable updateable in updateablesToRemove)
+        {
+            updateables.Remove(updateable);
+        }
+        updateablesToRemove.Clear();
+
     }
 
     private void FixedUpdate()
     {
-        foreach(var fixedUpdateable in fixedUpdateableList)
+        foreach(IFixedUpdateable fixedUpdateable in fixedUpdateables)
         {
-            fixedUpdateable.FixedUpdate(Time.deltaTime);
+            fixedUpdateable.FixedUpdate(Time.fixedDeltaTime);
         }
+        foreach(IFixedUpdateable fixedUpdatable in fixedUpdateablesToRemove)
+        {
+            fixedUpdateables.Remove(fixedUpdatable);
+        }
+        fixedUpdateablesToRemove.Clear ();
     }
 
     private void LateUpdate()
     {
-        foreach (var lateUpdateable in lateUpdateableList)
+        foreach (ILateUpdateable lateUpdateable in lateUpdateables)
         {
             lateUpdateable.LateUpdate(Time.deltaTime);
         }
+        foreach(ILateUpdateable lateUpdateable in lateUpdateablesToRemove)
+        {
+            lateUpdateables.Remove(lateUpdateable);
+        }
+        lateUpdateablesToRemove.Clear();
     }
 
-    public void Register()
+    public void RegisterUpdateable( IUpdateable updateable)
     {
         // Logica de registro. Lo hacemos directo por lista o por eventos?
+        updateables.Add(updateable);
     }
 
-    public void Unregister() 
+    public void UnregisterUpdateable(IUpdateable updateable) 
     {
-
+        updateablesToRemove.Add(updateable);
     }
+
+    public void RegisterFixedUpdateable(IFixedUpdateable fixedUpdateable)
+    {
+        // Logica de registro. Lo hacemos directo por lista o por eventos?
+        fixedUpdateables.Add(fixedUpdateable);
+    }
+
+    public void UnregisterFixedUpdateable(IFixedUpdateable fixedUpdateable)
+    {
+        fixedUpdateablesToRemove.Add(fixedUpdateable);
+    }
+
+    public void RegisterLateUpdateable(ILateUpdateable lateUpdateable)
+    {
+        // Logica de registro. Lo hacemos directo por lista o por eventos?
+        lateUpdateables.Add(lateUpdateable);
+    }
+
+    public void UnregisterLateUpdateable(ILateUpdateable lateUpdateable)
+    {
+        lateUpdateables.Add(lateUpdateable);
+    }
+
 
 }
